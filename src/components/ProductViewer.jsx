e36df4dd6150dc2ede9105483733671eed30d22b
BeingRelentless <amankumarsjc@gmail.com>
2026-03-20 15:34:32 +0530

    canvas

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,31 +1,62 @@
 import React from "react";
-import { useMacbookStore } from "../context/MacBookStoreContext";
+import { useMacBookStore } from "../context/MacBookStoreContext";
+import clsx from "clsx";
+import { Box } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 
 const ProductViewer = () => {
-  const { color, setColor, scale, setScale, reset } = useMacbookStore();
+  const { color, setColor, scale, setScale, reset } = useMacBookStore();
   return (
     <section id="product-viewer">
       <h2>Take a closer look</h2>
 
       <div className="controls">
-        <p className="info">MacbookPro 16" in Silver / Space Black</p>
+        <p className="info">
+          MacbookPro 16" in {color === "#adb5bd" ? "Silver" : "Space Black"}
+        </p>
 
         <div className="flex-center gap-5 mt-5">
           <div className="color-control">
-            <div className="bg-neutral-300"></div>
-            <div className="bg-neutral-900"></div>
+            <div
+              onClick={() => setColor("#adb5bd")}
+              className={clsx(
+                "bg-neutral-300",
+                color === "#adb5bd" && "active",
+              )}
+            ></div>
+            <div
+              onClick={() => setColor("#2e2c2e")}
+              className={clsx(
+                "bg-neutral-900",
+                color === "#2e2c2e" && "active",
+              )}
+            ></div>
           </div>
 
           <div className="size-control">
-            <div>
+            <div
+              onClick={() => {
+                setScale(0.06);
+              }}
+              className={clsx(scale === 0.06 ? "bg-white text-black" : "bg-transparent")}
+            >
               <p>14"</p>
             </div>
-            <div>
+            <div
+              onClick={() => {
+                setScale(0.08);
+              }}
+              className={clsx(scale === 0.08 ? "bg-white text-black" : "bg-transparent")}
+            >
               <p>16"</p>
             </div>
           </div>
         </div>
       </div>
+      
+      <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50 , near: 0.1, far: 1000}}>
+          <Box position={[0, 0, 0]} scale={10 * scale} material-color={color} />
+      </Canvas>
     </section>
   );
 };
